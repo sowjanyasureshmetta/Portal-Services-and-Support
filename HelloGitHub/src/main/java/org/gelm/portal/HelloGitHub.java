cdb660d1441d44f4d52660f5981ef6618d792db2
sureshkrishnaGE <sureshkrishna0517@gmail.com>
2020-03-22 00:20:01 +0800

    Added maven plugin

--- HelloGitHub/src/main/java/org/gelm/portal/HelloGitHub.java
+++ HelloGitHub/src/main/java/org/gelm/portal/HelloGitHub.java
@@ -3,7 +3,7 @@ package org.gelm.portal;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git Hub Project");
+		System.out.println("Hi , Hello Git Hub Project");
 	}
 	
 }
